b955b301ab4bf2d5483ba079a6037542c2fd6e69
angelikatr <angelika.trep@gmail.com>
2017-03-23 08:08:50 +0100

    idk

---
+++ WinSnake/Rechteck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinSnake
+{
+    class Rechteck
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Rechteck(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Rechteck()
+        {
+            X = 0;
+            Y = 0;
+        }
+    }
+}
--- WinSnake/Update.cs
+++ WinSnake/Update.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
 
 namespace WinSnake
 {
     class Update
     {
+        Random rnd = new Random(DateTime.Now.Millisecond);
+        Graphics g;
+        public int intEssenY { get; set; }
+        public int intEssenX { get; set; }
+        public List<Rechteck> snakelist { get; set; }
+        
+        
+        public void UpdateAll(PictureBox pb,int intx, int inty, int intboxsize)
+        {
+            g = pb.CreateGraphics();
+            g.FillRectangle(Brushes.White,0,0,450,450);
+
+            for (int i = 0; i < snakelist.Count; i++)
+            {
+                //Schlange
+                g.FillRectangle(Brushes.Pink,snakelist[i].X=intx * intboxsize, snakelist[i].Y = inty * intboxsize, intboxsize, intboxsize);
+            } 
+            
+            //Essen
+            g.FillRectangle(Brushes.Peru,intEssenX * intboxsize,intEssenY * intboxsize, intboxsize, intboxsize);
+
+        }
+        
+        public void Schlange(PictureBox pb,int intx, int inty, int intboxsize, int intschlangenlaenge)
+        {
+            g = pb.CreateGraphics();
+            g.FillRectangle(Brushes.Pink, intx *intboxsize, inty*intboxsize , intboxsize, intboxsize);
+        }
+       
+        public void UpdateEssen(PictureBox pb, int intboxsize)
+        {
+            g = pb.CreateGraphics();
+            intEssenX = rnd.Next(0, 30);
+            intEssenY = rnd.Next(0, 30);
+            g.FillRectangle(Brushes.Peru, intEssenX * intboxsize, intEssenY * intboxsize, intboxsize, intboxsize);
+
+        }
+
+       
     }
 }
